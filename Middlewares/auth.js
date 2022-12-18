e5c5111319cf4dca99556c25fc722dee85760c6d
ssjisan <sakibfset13@gmail.com>
2022-12-18 18:07:53 +0600

    token set in header

---
+++ Middlewares/auth.js
@@ -0,0 +1,4 @@
+export const requireSignin = (req, res, next) => {
+  console.log("REQ HEADERS=>", req.headers);
+  next()
+};
--- Routes/auth.js
+++ Routes/auth.js
@@ -1,8 +1,14 @@
 import express from "express";
-import {register,login} from "../Controllers/auth.js";
+import { register, login } from "../Controllers/auth.js";
+import { requireSignin } from "../Middlewares/auth.js";
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+
+// Secrect Route
+router.get("/secret", requireSignin, (req, res) => {
+  res.json({ message: "Secret Route" });
+});
 
 export default router;
